4b7404d57c1a999f764b080cdd20dcfbfdf22e6a
listiani13 <listiliz13@gmail.com>
2019-02-03 20:59:48 +0700

    Add Text core UI

---
+++ src/constants/text.js
@@ -0,0 +1,11 @@
+// @flow
+
+const FONT_SIZE = {
+  xsmall: 13,
+  small: 15,
+  medium: 18,
+  large: 28,
+  xlarge: 36,
+};
+
+export {FONT_SIZE};
---
+++ src/core-ui/Text.js
@@ -0,0 +1,55 @@
+// @flow
+
+import React from 'react';
+import {Text as TextComp, StyleSheet} from 'react-native';
+import {FONT_SIZE} from '../constants/text';
+import {BAHAMA_BLUE} from '../constants/color';
+
+type Props = {
+  size?: 'xsmall' | 'small' | 'medium' | 'large' | 'xlarge',
+  weight?: 'light' | 'reg' | 'bold' | 'black',
+  primary?: boolean,
+  color?: string,
+  style?: StyleSet,
+  fontWeight?: '100' | '200' | '300' | '400' | '500' | 'bold',
+  children: ReactNode | string,
+};
+
+function Text(props: Props) {
+  let {
+    primary,
+    weight,
+    style,
+    color,
+    size,
+    children,
+    fontWeight,
+    ...otherProps
+  } = props;
+  let fontSize = FONT_SIZE[size || 'medium'];
+  let fontStyle = [
+    styles.default,
+    {fontSize, fontWeight},
+    {color},
+    primary && styles.primary,
+    style && style,
+  ];
+  return (
+    <TextComp style={fontStyle} {...otherProps}>
+      {children}
+    </TextComp>
+  );
+}
+
+export default Text;
+
+const styles = StyleSheet.create({
+  default: {
+    fontSize: FONT_SIZE['medium'],
+    fontWeight: '300',
+    color: BAHAMA_BLUE,
+  },
+  primary: {
+    color: BAHAMA_BLUE,
+  },
+});
