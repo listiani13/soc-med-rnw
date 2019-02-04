513aab2ce5b60c0de34705571c862066b55df4c3
listiani13 <listiliz13@gmail.com>
2019-02-04 14:03:39 +0700

    Add package to deploy to github pages

--- src/routes/MainRoute.js
+++ src/routes/MainRoute.js
@@ -1,18 +1,18 @@
 // @flow
 import React, {Component} from 'react';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import {HashRouter, Route, Switch} from 'react-router-dom';
 import UserList from '../features/user/screens/UserListScene';
 import UserProfile from '../features/user/screens/UserProfileScene';
 
 export default class MainRoute extends Component<{}> {
   render() {
     return (
-      <BrowserRouter>
+      <HashRouter>
         <Switch>
           <Route path="/" exact strict component={UserList} />
           <Route path="/user/:id" exact strict component={UserProfile} />
         </Switch>
-      </BrowserRouter>
+      </HashRouter>
     );
   }
 }
